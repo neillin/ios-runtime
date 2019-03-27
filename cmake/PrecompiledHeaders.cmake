9e655852f6440ae5785143d6d6a225065ea12dc1
Martin Bektchiev <martin.bektchiev@progress.com>
2019-03-27 18:27:49 +0200

    chore(build): Disable precompiled headers when using ccache
    Otherwise all NativeScript source files are rebuilt everytime when the
    PCH file is regenerated. Which is done on every build.

--- cmake/PrecompiledHeaders.cmake
+++ cmake/PrecompiledHeaders.cmake
@@ -1,6 +1,8 @@
 macro(SetPrecompiledHeader _target _header)
-    set_target_properties(${_target} PROPERTIES
-        XCODE_ATTRIBUTE_GCC_PREFIX_HEADER "${CMAKE_CURRENT_SOURCE_DIR}/${_header}"
-        XCODE_ATTRIBUTE_GCC_PRECOMPILE_PREFIX_HEADER "YES"
-    )
+    if (NATIVESCRIPT_USE_PREFIX_HEADER)
+        set_target_properties(${_target} PROPERTIES
+            XCODE_ATTRIBUTE_GCC_PREFIX_HEADER "${CMAKE_CURRENT_SOURCE_DIR}/${_header}"
+            XCODE_ATTRIBUTE_GCC_PRECOMPILE_PREFIX_HEADER "YES"
+        )
+    endif()
 endmacro()
